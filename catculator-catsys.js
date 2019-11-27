d5caa93b0db8a1e0a878751d8b1c3e858625a82a
AcheZeta <ache.mariana@gmail.com>
2019-11-26 21:24:22 -0600

    TODO-List

--- catculator-catsys.js
+++ catculator-catsys.js
@@ -3,11 +3,29 @@ import {
   LitElement
 } from 'lit-element';
 import style from './catculator-catsys-styles.js';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
+
+const VisibilityFilters = {
+  SHOW_ALL: 'All',
+  SHOW_ACTIVE: 'Active',
+  SHOW_COMPLETED: 'Completed'
+};
 
 class CatculatorCatsys extends LitElement {
+
   static get properties() {
     return {
-      hello: {
+      todos: {
+        type: Array
+      },
+      filter: {
+        type: String
+      },
+      task: {
         type: String
       }
     };
@@ -19,15 +37,60 @@ class CatculatorCatsys extends LitElement {
 
   constructor() {
     super();
-    this.hello = 'Hello';
+    this.todos = [];
+    this.filter = VisibilityFilters.SHOW_ALL;
+    this.task = '';
   }
 
   render() {
     return html `
-        <p>Some static DOM</p>
-        <h2>${this.hello} catculator-catsys</h2>
-      `;
+        <div class="input-layout" @keyup="${this.shortcutListener}">
+          <vaadin-text-field placeholder="Task" value="${this.task}" @change="${this.updateTask}">
+          </vaadin-text-field>
+
+          <vaadin-button theme="primary"
+        @click = "${this.addTodo}">
+          Add Todo </vaadin-button> </div>
+
+          
+          <div class="todo-list">
+          ${this.todos.map(todo => html`
+          <div class="todo-item">
+          <vaadin-checkbox ?checked="${todo.complete}" @change="${event => this.updateTodoStatus}">
+          ${todo.task}
+          </vaadin-checkbox >
+          </div>
+          `)}
+          </div>
+          `;
   }
+
+
+  addTodo() {
+    if (this.task) {
+      this.todos = [...this.todos, {
+        task: this.task,
+        complete: false
+      }];
+      this.task = '';
+    }
+  }
+
+  shortcutListener(event) {
+    if (event.key === 'Enter') {
+      this.addTodo();
+    }
+  }
+
+  updateTask(event) {
+    this.task = event.target.value;
+  }
+
 }
+
+
+
+
+
 
 window.customElements.define("catculator-catsys", CatculatorCatsys);
