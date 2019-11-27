424ac471490569726ef0cb804367aba45f98d9b8
AcheZeta <ache.mariana@gmail.com>
2019-11-27 12:34:02 -0600

    FEAT: Dinamyc Button

--- catculator-catsys.js
+++ catculator-catsys.js
@@ -9,24 +9,16 @@ import '@vaadin/vaadin-checkbox';
 import '@vaadin/vaadin-radio-button/vaadin-radio-button';
 import '@vaadin/vaadin-radio-button/vaadin-radio-group';
 
-const VisibilityFilters = {
-  SHOW_ALL: 'All',
-  SHOW_ACTIVE: 'Active',
-  SHOW_COMPLETED: 'Completed'
-};
 
 class CatculatorCatsys extends LitElement {
 
   static get properties() {
     return {
-      todos: {
+      result: {
+        type: Number
+      },
+      digit: {
         type: Array
-      },
-      filter: {
-        type: String
-      },
-      task: {
-        type: String
       }
     };
   }
@@ -37,60 +29,56 @@ class CatculatorCatsys extends LitElement {
 
   constructor() {
     super();
-    this.todos = [];
-    this.filter = VisibilityFilters.SHOW_ALL;
-    this.task = '';
+    this.result = 0;
+    this.digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   }
 
   render() {
     return html `
-        <div class="input-layout" @keyup="${this.shortcutListener}">
-          <vaadin-text-field placeholder="Task" value="${this.task}" @change="${this.updateTask}">
-          </vaadin-text-field>
-
-          <vaadin-button theme="primary"
-        @click = "${this.addTodo}">
-          Add Todo </vaadin-button> </div>
-
-          
-          <div class="todo-list">
-          ${this.todos.map(todo => html`
-          <div class="todo-item">
-          <vaadin-checkbox ?checked="${todo.complete}" @change="${event => this.updateTodoStatus}">
-          ${todo.task}
-          </vaadin-checkbox >
-          </div>
-          `)}
-          </div>
-          `;
+    <div id="display">
+      <h1 id="display__numbers"> ${this.result} </h1>
+    </div>
+    <div class="keyboard">
+  ${this.digit.map(digit => html `
+    <button class="digit" id="${digit}">
+      ${digit}
+      </button>
+    `)
+  } </div>
+   `;
   }
-
-
-  addTodo() {
-    if (this.task) {
-      this.todos = [...this.todos, {
-        task: this.task,
-        complete: false
-      }];
-      this.task = '';
-    }
-  }
-
-  shortcutListener(event) {
-    if (event.key === 'Enter') {
-      this.addTodo();
-    }
-  }
-
-  updateTask(event) {
-    this.task = event.target.value;
-  }
-
 }
 
 
 
 
+// <
+// div class = "input-layout"
+// @keyup = "${this.shortcutListener}" >
+//   <
+//   vaadin - text - field placeholder = "Task"
+// value = "${this.task}"
+// @change = "${this.updateTask}" >
+//   <
+//   /vaadin-text-field>
 
+//   <
+//   vaadin - button theme = "primary"
+// @click = "${this.addTodo}" >
+//   Add Todo < /vaadin-button> </div >
+
+
+//   <
+//   div class = "todo-list" >
+//   $ {
+//     this.todos.map(todo => html `
+//           <div class="todo-item">
+//           <vaadin-checkbox ?checked="${todo.complete}" @change="${event => this.updateTodoStatus}">
+//           ${todo.task}
+//           </vaadin-checkbox >
+//           </div>
+//           `)
+//   } <
+//   /div>
 
 window.customElements.define("catculator-catsys", CatculatorCatsys);
