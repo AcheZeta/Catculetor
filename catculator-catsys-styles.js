3a070013b516d1d8962f24448dc880658088e776
AcheZeta <ache.mariana@gmail.com>
2019-11-27 23:04:36 -0600

    Styles Css

--- catculator-catsys-styles.js
+++ catculator-catsys-styles.js
@@ -1,6 +1,8 @@
-import { css } from 'lit-element';
+import {
+  css
+} from 'lit-element';
 
-export default css`
+export default css `
 :host {
   display: inline-block;
   box-sizing: border-box; }
@@ -11,4 +13,15 @@ export default css`
 *, *:before, *:after {
   box-sizing: inherit;
   font-family: inherit; }
+
+.container{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 5px;
+  grid-row-gap: 5px;
+}
+.input{
+  grid-area: 1 / 1 / 6 / 4
+}
 `;
